3a4c9159f8be352996c9665145bc4a5b8c43e80d
mauricioarcez <mauricioarcez23@gmail.com>
2024-09-05 18:08:39 -0300

    creacion de un requirement produccion y otro local

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Usa el puerto definido en PORT o el 8000 por defecto
+    uvicorn.run(app, host="0.0.0.0", port=port)
