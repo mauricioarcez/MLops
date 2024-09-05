4655c780c6e8151f13e92cdd6928dfd98f7d6d7e
mauricioarcez <mauricioarcez23@gmail.com>
2024-09-05 19:00:24 -0300

    cambios para render

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
-import os
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Usa el puerto definido en PORT o el 8000 por defecto
-    uvicorn.run(app, host="0.0.0.0", port=port)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
